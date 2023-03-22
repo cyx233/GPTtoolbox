f65a7236aa6bbd0ad0e39ca092646cc09877ecf7
Yuxiang Chen <yuc129@ucsd.edu>
2023-03-22 00:30:38 -0700

    try-cache

--- src/main.py
+++ src/main.py
@@ -3,11 +3,11 @@ from gui import LauncherWindow
 import sys
 
 if __name__ == "__main__":
-    # try:
-    app = QApplication()
-    launcher_window = LauncherWindow()
-    launcher_window.show()
-    sys.exit(app.exec_())
-    # except Exception as e:
-    #     print(f"An error occurred: {e}")
-    #     sys.exit(1)
+    try:
+        app = QApplication()
+        launcher_window = LauncherWindow()
+        launcher_window.show()
+        sys.exit(app.exec_())
+    except Exception as e:
+        print(e.with_traceback())
+        sys.exit(1)
