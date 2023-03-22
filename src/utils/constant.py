084024e8edafa618d464da69e2f7ad8a67a59923
Yuxiang Chen <yuc129@ucsd.edu>
2023-03-21 18:27:39 -0700

    enviroment variable

--- src/utils/constant.py
+++ src/utils/constant.py
@@ -1,5 +1,13 @@
 import os
 import json
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+API_KEY = os.environ.get('API_KEY')
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
 src_dir = os.path.normpath(os.path.join(os.path.dirname(__file__), ".."))
 save_dir = os.path.normpath(os.path.join(src_dir, "../saves"))
 
